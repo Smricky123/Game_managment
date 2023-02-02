71ac6f41624be6c2a87e6ed02010786a5c5136db
Ricky <18-38861-3@student.aiub.edu>
2023-02-02 20:15:55 +0600

    Basic of get, param and require parameters

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get } from '@nestjs/common';
+import { Body, Post, Req, Res } from '@nestjs/common/decorators';
+import { Param, Query } from '@nestjs/common/decorators/http/route-params.decorator';
+import { Request, Response } from 'express';
+import { CreateUserDto } from '../DTOS/CreateUser.dtos';
+//creating two routes name users and post
+
+@Controller('users')
+export class UsersController {
+    //General get withiut route
+    @Get()
+    getUsers(){
+       return[
+       {username:'Ricky' ,
+       Gmail:'@com'}];
+    }
+
+    @Get('post/comment')
+    getUserPost(){
+        return [
+        {ID1:'Designer', 
+        ID2:'Developer',
+        comment: ['Alright'],},
+    ];
+    }
+
+    //route for push request
+    @Post('create')
+    creatUser(@Body()userdata:CreateUserDto){
+        console.log(userdata);
+        return{};
+
+    }
+
+    //route for get
+    @Get(':id/:postId')
+    getUserById(@Param('id') id : string, @Param('postId') postId : string){
+         console.log(id);
+         return{id,postId};
+    }
+
+    //route for query
+    @Get()
+    getUser(@Query('sortBy')sortBy: String){
+      console.log(sortBy);  
+    }
+
+}
